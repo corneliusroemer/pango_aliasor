ed8e87b18b5297b57aac81b37a73d46fc8e08ec9
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-06-26 21:17:56 +0200

    chore: add test for reading from file

--- tests/test_aliasor.py
+++ tests/test_aliasor.py
@@ -1,4 +1,26 @@
+from __future__ import unicode_literals
+
+import os
+from distutils import dir_util
+
 from pango_aliasor.aliasor import Aliasor
+from pytest import fixture
+
+
+@fixture
+def datadir(tmpdir, request):
+    '''
+    Fixture responsible for searching a folder with the same name of test
+    module and, if available, moving all contents to a temporary directory so
+    tests can use them freely.
+    '''
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir,str(tmpdir))
+
+    return tmpdir
 
 def test_pango_designation_init():
     aliasor = Aliasor()
@@ -34,3 +56,7 @@ def test_double_alias_compression():
 def test_double_alias_uncompression():
     aliasor = Aliasor()
     assert aliasor.uncompress('BE.1') == 'B.1.1.529.5.3.1.1'
+
+def test_read_from_file(datadir):
+    aliasor = Aliasor(datadir.join('alias_key.json'))
+    assert aliasor.compress('B.1.1.529.1') == 'BA.1'
