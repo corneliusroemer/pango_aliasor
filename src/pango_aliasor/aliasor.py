681b0bbb762cab12abf35c4074ab76486b8ab999
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-09-04 16:38:24 +0200

    feat: add partial compression with up_to and accepted_aliases

--- src/pango_aliasor/aliasor.py
+++ src/pango_aliasor/aliasor.py
@@ -47,6 +47,40 @@ class Aliasor:
             return unaliased + "." + name_split[1]
         else:
             return unaliased + "." + ".".join(name_split[1:])
+    
+    def partial_compress(self, name, up_to: int = 0, accepted_aliases: set = {}):
+        """
+        aliasor.partial_compress("B.1.1.529.3.1",up_to=1) # 'BA.3.1'
+        aliasor.partial_compress("B.1.1.529.3.1.1.2",up_to=1) # 'BA.3.1.1.2'
+
+        aliasor.partial_compress("B.1.1.529.3.1",accepted_aliases=["AY"]) # 'B.1.1.529.3.1'
+        aliasor.partial_compress("B.1.617.2",accepted_aliases=["AY"]) # 'AY.2'
+        """
+        # If accepted_aliases is passed without up_to set, then try out all possible values
+        name_split = name.split(".")
+        levels = len(name_split) - 1
+        indirections = (levels - 1) // 3
+
+        alias = name_split[0]
+
+        if up_to > 0:
+            if indirections <= up_to:
+                return self.compress(name)
+            to_alias = ".".join(name_split[0 : (3 * up_to + 1)])
+            alias = self.realias_dict[to_alias]
+        
+        # Compress at least till up_to, maybe further
+
+        # Check if levels beyond up_to (working backwards) are in accepted_aliases
+        if accepted_aliases is not {}:
+            for level in range(indirections,up_to,-1):
+                to_alias = ".".join(name_split[0 : (3 * level + 1)])
+                if to_alias in self.realias_dict.keys():
+                    if self.realias_dict[to_alias] in accepted_aliases:
+                        alias = self.realias_dict[to_alias]
+                        return alias + "." + ".".join(name_split[(3 * level + 1) :])
+
+        return alias + "." + ".".join(name_split[(3 * up_to + 1) :])
 
 
 # %%
